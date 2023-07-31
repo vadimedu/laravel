6252ebfdaa23ddacad49104b1d0c788914d65008
vadimedu <94390303+vadimedu@users.noreply.github.com>
2023-07-31 18:22:40 +0300

    Add files via upload

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Events\LoginEvent;
+use AutheticatesUsers;
+use App\Models\User;
 
 class LoginController extends Controller
 {
     public function login()
     {
        return view('auth.login');
+    }
+    public function authenticated(Request $request, $user) {
+        event(new LoginEvent($user));
     }
 
 }
